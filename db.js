5fb758eb252e6508e485efe1e1bc97ca7e2e53c2
Rolandsson <102300415+Rolandsson@users.noreply.github.com>
2022-12-16 10:05:40 +0100

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 /* DISCLAIMER: all rights reserved to creator of github repository https://github.com/igdev116/free-food-menus-api under ISC per package declaration */
 
-let db = {
+const db = {
   "bbqs": [
     {
       "id": "ribs-brisket-and-burnt-ends",
